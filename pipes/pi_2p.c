5ad2f76671ac1d3189bd2db53da25cb1a63ce5a4
Leonardo B. Tosin <40045069+TheLe0@users.noreply.github.com>
2019-04-23 22:54:18 -0300

    Create pi_2p.c

---
+++ pipes/pi_2p.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <unistd.h>
+
+/*---------------------------------------------------------------------*/
+double calcula_pi(double nr, double inicio, double salto){
+    double x, base, altura;
+    double i, pi;
+
+    base = 1.0 / nr;
+    pi = 0.0;
+
+    for( i=inicio ; i<=nr ;  i+= salto ) {
+        x = base * (( double)i - 0.5);
+        altura = 4.0 / (1.0 + x*x);
+        pi += base * altura;
+    }
+
+    return pi;
+
+
+}
+/*---------------------------------------------------------------------*/
+int main(int argc, char **argv){
+
+    double pi = 0, aux;
+    int pid;
+    double nr;
+    double i;
+    int p[2];
+
+    if ( argc != 2 ){
+        printf("%s <num_retangulos>\n", argv[0]);
+        exit(0);
+    }
+
+
+    nr = atof(argv[1]);
+
+    pipe(p);
+    pid = fork();
+
+    if (pid > 0) {
+        close(p[1]);
+        pi = calcula_pi(nr,2,2);
+        read(p[0], &aux, sizeof(double));
+        pi += aux;
+        close(p[0]);
+        printf("Pi = %.15f\n",pi);
+    } else {
+        close(p[0]);
+        pi = calcula_pi(nr,1,2);
+        write(p[1], &pi, sizeof(double));
+        close(p[1]);
+    }
+
+}
+/*---------------------------------------------------------------------*/
