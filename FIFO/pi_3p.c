02602b74afd79b9600dc9fa4c5c70e3cf5cceb64
Leonardo B. Tosin <40045069+TheLe0@users.noreply.github.com>
2019-04-27 17:52:52 -0300

    Create pi_3p.c

---
+++ FIFO/pi_3p.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define N 50
+
+/*---------------------------------------------------------------------*/
+double calcula_pi(double nr, double inicio, double salto){
+    double x, base, altura;
+    double i, pi;
+
+    base = 1.0 / nr;
+    pi = 0.0;
+
+    for( i=inicio ; i<=nr ;  i+= salto ) {
+        x = base * (( double)i - 0.5);
+        altura = 4.0 / (1.0 + x*x);
+        pi += base * altura;
+    }
+
+    return pi;
+
+
+}
+/*---------------------------------------------------------------------*/
+int main(int argc, char **argv){
+
+    double pi = 0, aux;
+    int pid, fd1, fd2;
+    double nr;
+    double i;
+    char fifo1[N] = "fifo1", fifo2[N] = "fifo2";
+
+    if ( argc != 2 ){
+        printf("%s <num_retangulos>\n", argv[0]);
+        exit(0);
+    }
+
+
+    nr = atof(argv[1]);
+
+    pid = fork();
+    mkfifo(fifo1, 0666);
+    mkfifo(fifo2, 0666);
+
+    if (pid > 0) {
+        pid = fork();
+        if (pid > 0) {
+            pi = calcula_pi(nr, 3, 3); 
+            fd1 = open(fifo1, O_RDONLY);
+            read(fd1, &aux, sizeof(double));
+            pi += aux;
+            fd2 = open(fifo2, O_RDONLY);
+            read(fd2, &aux, sizeof(double));
+            pi += aux;
+            printf("Pi = %.15f\n",pi);
+            unlink(fifo1);
+            unlink(fifo2);
+        } else {
+            fd2 = open(fifo2, O_WRONLY);
+            pi = calcula_pi(nr, 2, 3);
+            write(fd2, &pi, sizeof(double));   
+            close(fd2);
+        }
+    } else {
+        fd1 = open(fifo1, O_WRONLY);
+        pi = calcula_pi(nr, 1, 3);
+        write(fd1, &pi, sizeof(double));   
+        close(fd1);  
+    }
+
+}
+/*---------------------------------------------------------------------*/
